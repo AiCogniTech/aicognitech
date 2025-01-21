8af9145ff9ede4daf82e4327e2aaa0da44162767
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2025-01-21 13:57:40 +0500

    Add Image & change title

--- src/components/contact/ContactHero.tsx
+++ src/components/contact/ContactHero.tsx
@@ -7,9 +7,8 @@ const ContactHero = () => {
             <div className="absolute inset-0 bg-contact-hero bg-cover bg-center w-full h-full z-[-1]" />
             <div className="flex-center flex-col text-center gap-10 md:gap-5 relative bottom-10 z-10 w-[90vw] md:w-[70vw] xl:w-[55vw] font-helvetica text-primary">
                 <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">
-                    Your Partner In Creating <br className="hidden lg:block" />
-                    Intelligent <span className="text-secondary">Solutions</span> <br className='hidden xl:block' />
-                    For A Better World
+                    Partner & Grow: AI for <br className="hidden lg:block" />
+                    <span className="text-secondary">Health & Wellness</span> <br className='hidden xl:block' />
                 </h1>
                 <p className='text-base md:text-lg lg:text-xl font-light'>
                     Custom software design, development & AI solutions for healthcare with a vision to change the world.
