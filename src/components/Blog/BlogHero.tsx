3890d20cace0e3cd4ee089ba55e01ac9d667fafc
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2025-01-21 16:40:45 +0500

    Some Minor Changes

--- src/components/Blog/BlogHero.tsx
+++ src/components/Blog/BlogHero.tsx
@@ -6,7 +6,7 @@ const BlogHero = () => {
     return (
         <section className="flex flex-col-reverse lg:flex-row justify-center lg:justify-start lg:items-center px-5 md:px-10 lg:px-16 w-full h-[80vh] lg:h-auto lg:min-h-[80vh] md:mb-36 xl:mb-0">
             <div className='absolute top-0 left-0 bg-primary/50 w-full h-full object-cover z-[-1] opacity-95' />
-            <div className="flex-between flex-col lg:flex-row gap-10 md:gap-20 relative z-10 font-helvetica w-full py-6 md:py-12 px-8 md:px-14">
+            <div className="flex-between flex-col lg:flex-row gap-10 md:gap-20 relative z-10 font-helvetica w-full py-6 md:py-12 px-8 md:px-14 lg:bottom-10">
                 <div className='flex-1 flex flex-col gap-6'>
                     <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-[50px] font-bold">
                         Newsletter  -  Stay Ahead of the Curve: Join AiCogniTech
